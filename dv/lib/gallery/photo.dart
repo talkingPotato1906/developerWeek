b1cb4202b78cad0888f5220df7a5165369dd5422
yabttuck <2416833@sookmyung.ac.kr>
2025-01-31 00:13:43 +0900

    ✨ 보관함/갤러리 기능 추가 및 수정
    1. 보관함에서 갤러리 추가 기능

--- dv/lib/gallery/photo.dart
+++ dv/lib/gallery/photo.dart
@@ -1,8 +1,8 @@
-import 'package:dv/settings/language/language_provider.dart';
 import 'package:flutter/material.dart';
 import 'package:image_picker/image_picker.dart';
 import 'package:provider/provider.dart';
-import 'image_provider.dart'; // ImageProviderClass 추가
+
+import 'image_provider.dart';
 
 class PhotoPage extends StatefulWidget {
   const PhotoPage({super.key});
@@ -56,7 +56,6 @@ class _PhotoPageState extends State<PhotoPage> {
       );
 
       if (result != null) {
-        // ImageProviderClass를 사용하여 이미지 추가
         Provider.of<ImageProviderClass>(context, listen: false).addImage({
           "image": bytes,
           "title": result["title"]!,
@@ -66,49 +65,189 @@ class _PhotoPageState extends State<PhotoPage> {
     }
   }
 
-  void _showImageContent(Map<String, dynamic> imageData) {
+  void _showImageContent(BuildContext context, int index) {
+    final provider = Provider.of<ImageProviderClass>(context, listen: false);
+    final imageData = provider.images[index];
+
     showDialog(
       context: context,
       builder: (context) {
-        return Dialog(
-          shape: RoundedRectangleBorder(
-            borderRadius: BorderRadius.circular(16.0),
-          ),
-          child: Container(
-            width: MediaQuery.of(context).size.width * 0.8,
-            height: MediaQuery.of(context).size.height * 0.8,
-            padding: EdgeInsets.all(16.0),
-            child: Row(
-              children: [
-                SizedBox(
-                  width: MediaQuery.of(context).size.width * 0.3,
-                  height: MediaQuery.of(context).size.height * 0.8,
-                  child: Image.memory(
-                    imageData["image"],
-                    fit: BoxFit.cover,
-                  ),
-                ),
-                SizedBox(width: 16.0),
-                Expanded(
-                  child: Column(
-                    crossAxisAlignment: CrossAxisAlignment.start,
-                    children: [
-                      SizedBox(
-                          height: MediaQuery.of(context).size.height * 0.02),
-                      Text(
-                        imageData["title"],
-                        style: TextStyle(
-                            fontWeight: FontWeight.bold, fontSize: 18.0),
+        return Consumer<ImageProviderClass>(
+          builder: (context, provider, child) {
+            bool isSelected = provider.selectedImages.contains(imageData);
+
+            return Dialog(
+              shape: RoundedRectangleBorder(
+                borderRadius: BorderRadius.circular(16.0),
+              ),
+              child: Container(
+                width: MediaQuery.of(context).size.width * 0.8,
+                height: MediaQuery.of(context).size.height * 0.8,
+                padding: EdgeInsets.all(16.0),
+                child: Column(
+                  crossAxisAlignment: CrossAxisAlignment.start,
+                  children: [
+                    Expanded(
+                      child: Row(
+                        children: [
+                          SizedBox(
+                            width: MediaQuery.of(context).size.width * 0.3,
+                            height: MediaQuery.of(context).size.height * 0.8,
+                            child: Image.memory(
+                              imageData["image"],
+                              fit: BoxFit.cover,
+                            ),
+                          ),
+                          SizedBox(width: 16.0),
+                          Expanded(
+                            child: Column(
+                              crossAxisAlignment: CrossAxisAlignment.start,
+                              children: [
+                                SizedBox(height: 16),
+                                Text(
+                                  imageData["title"],
+                                  style: TextStyle(
+                                      fontWeight: FontWeight.bold,
+                                      fontSize: 18.0),
+                                ),
+                                SizedBox(height: 16),
+                                Text(imageData["content"]),
+                              ],
+                            ),
+                          ),
+                        ],
                       ),
-                      SizedBox(
-                          height: MediaQuery.of(context).size.height * 0.02),
-                      Text(imageData["content"]),
-                    ],
-                  ),
+                    ),
+                    Row(
+                      mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                      children: [
+                        Expanded(
+                          child: InkWell(
+                            onTap: () {
+                              if (!isSelected &&
+                                  provider.selectedImages.length >= 9) {
+                                _showMaxSelectionWarning(context);
+                              } else {
+                                provider.toggleGalleryImage(imageData);
+                              }
+                            },
+                            child: Row(
+                              children: [
+                                Checkbox(
+                                  value: isSelected,
+                                  onChanged: (value) {
+                                    if (!isSelected &&
+                                        provider.selectedImages.length >= 9) {
+                                      _showMaxSelectionWarning(context);
+                                    } else {
+                                      provider.toggleGalleryImage(imageData);
+                                    }
+                                  },
+                                ),
+                                Text("갤러리에 추가"),
+                              ],
+                            ),
+                          ),
+                        ),
+                        Row(
+                          children: [
+                            ElevatedButton.icon(
+                              onPressed: () {
+                                Navigator.of(context).pop();
+                                _editImageContent(context, index);
+                              },
+                              icon: Icon(Icons.edit, color: Colors.white),
+                              label: Text("수정"),
+                              style: ElevatedButton.styleFrom(
+                                backgroundColor: Colors.blue,
+                              ),
+                            ),
+                            SizedBox(width: 8.0),
+                            ElevatedButton.icon(
+                              onPressed: () {
+                                provider.removeImage(index);
+                                Navigator.of(context).pop();
+                              },
+                              icon: Icon(Icons.delete, color: Colors.white),
+                              label: Text("삭제"),
+                              style: ElevatedButton.styleFrom(
+                                backgroundColor: Colors.red,
+                              ),
+                            ),
+                          ],
+                        ),
+                      ],
+                    ),
+                  ],
                 ),
-              ],
-            ),
+              ),
+            );
+          },
+        );
+      },
+    );
+  }
+
+  void _editImageContent(BuildContext context, int index) {
+    final provider = Provider.of<ImageProviderClass>(context, listen: false);
+    final imageData = provider.images[index];
+
+    TextEditingController titleController =
+        TextEditingController(text: imageData["title"]);
+    TextEditingController contentController =
+        TextEditingController(text: imageData["content"]);
+
+    showDialog(
+      context: context,
+      builder: (context) {
+        return AlertDialog(
+          title: Text("제목과 내용 수정"),
+          content: Column(
+            mainAxisSize: MainAxisSize.min,
+            children: [
+              TextField(
+                controller: titleController,
+                decoration: InputDecoration(hintText: "제목을 입력하세요"),
+              ),
+              TextField(
+                controller: contentController,
+                decoration: InputDecoration(hintText: "내용을 입력하세요"),
+              ),
+            ],
           ),
+          actions: [
+            TextButton(
+              onPressed: () {
+                provider.updateImage(
+                    index, titleController.text, contentController.text);
+                Navigator.of(context).pop(); // ✅ 다이얼로그 닫기
+
+                // ✅ UI를 먼저 갱신한 후 다이얼로그 다시 열기
+                WidgetsBinding.instance.addPostFrameCallback((_) {
+                  _showImageContent(context, index);
+                });
+              },
+              child: Text("저장"),
+            ),
+          ],
+        );
+      },
+    );
+  }
+
+  void _showMaxSelectionWarning(BuildContext context) {
+    showDialog(
+      context: context,
+      builder: (context) {
+        return AlertDialog(
+          title: Text("갤러리 선택 제한"),
+          content: Text("최대 9개의 전시품만 선택할 수 있습니다."),
+          actions: [
+            TextButton(
+              onPressed: () => Navigator.of(context).pop(),
+              child: Text("확인"),
+            ),
+          ],
         );
       },
     );
@@ -116,21 +255,14 @@ class _PhotoPageState extends State<PhotoPage> {
 
   @override
   Widget build(BuildContext context) {
-    // 이미지를 공유하는 위젯
-    final images = Provider.of<ImageProviderClass>(context).images;
-    final languageProvider = Provider.of<LanguageProvider>(context);
+    final provider = Provider.of<ImageProviderClass>(context);
+    final images = provider.images;
 
     return Scaffold(
       appBar: AppBar(
-        automaticallyImplyLeading: false,
-        title: Text(languageProvider.getLanguage(message: "나의 보관함")),
-        actions: [
-          IconButton(
-            icon: Icon(Icons.add_photo_alternate),
-            onPressed: _pickImageAndComment,
-          ),
-        ],
-      ),
+        title: Text("나의 보관함"),
+      ), // ✅ 상단 이미지 추가 버튼 삭제
+
       body: Padding(
         padding: EdgeInsets.all(8.0),
         child: GridView.builder(
@@ -142,36 +274,37 @@ class _PhotoPageState extends State<PhotoPage> {
           itemCount: images.length,
           itemBuilder: (context, index) {
             final imageData = images[index];
+            bool isSelected = provider.selectedImages.contains(imageData);
+
             return GestureDetector(
-              onTap: () => _showImageContent(imageData),
-              child: ClipRRect(
-                borderRadius: BorderRadius.circular(8.0),
-                child: Stack(
-                  fit: StackFit.expand,
-                  children: [
-                    Image.memory(
+              onTap: () => _showImageContent(context, index),
+              child: Stack(
+                children: [
+                  ClipRRect(
+                    borderRadius: BorderRadius.circular(8.0),
+                    child: Image.memory(
                       imageData["image"],
                       fit: BoxFit.cover,
                     ),
+                  ),
+                  if (isSelected)
                     Positioned(
-                      bottom: 8.0,
-                      left: 8.0,
-                      right: 8.0,
-                      child: Container(
-                        color: Colors.black.withOpacity(0.6),
-                        child: Text(
-                          imageData["title"],
-                          style: TextStyle(color: Colors.white),
-                          overflow: TextOverflow.ellipsis,
-                        ),
-                      ),
+                      //갤러리에 추가 시 체크버튼(왼쪽아래래)
+                      bottom: 8,
+                      left: 8,
+                      child: Icon(Icons.check_circle, color: Colors.green),
                     ),
-                  ],
-                ),
+                ],
               ),
             );
           },
         ),
+      ),
+
+      // ✅ 하단 FloatingActionButton만 유지
+      floatingActionButton: FloatingActionButton(
+        onPressed: _pickImageAndComment,
+        child: Icon(Icons.add),
       ),
     );
   }
