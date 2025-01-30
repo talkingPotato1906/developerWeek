25f345c34587e22a5db2d7b66deee3c6e5db0a21
Eunjin <eunjinlee0128@gmail.com>
2025-01-30 23:03:00 +0900

    🎨 애니메이션 수정
    아래에서 올라오는 애니메이션에서 나타나는 애니메이션으로 변경

--- dv/lib/title/title_screen.dart
+++ dv/lib/title/title_screen.dart
@@ -2,19 +2,17 @@ import 'package:dv/settings/theme/color_palette.dart';
 import 'package:dv/settings/theme/theme_provider.dart';
 import 'package:flutter/material.dart';
 import 'package:provider/provider.dart';
-import 'package:dv/main.dart'; // 홈 화면이 있는 main.dart 파일 import
+import 'package:dv/main.dart'; // 홈 화면 import
 
-class ImageRiseAnimation extends StatefulWidget {
+class ImageFadeInAnimation extends StatefulWidget {
   @override
-  ImageRiseAnimationState createState() => ImageRiseAnimationState();
+  ImageFadeInAnimationState createState() => ImageFadeInAnimationState();
 }
 
-class ImageRiseAnimationState extends State<ImageRiseAnimation>
+class ImageFadeInAnimationState extends State<ImageFadeInAnimation>
     with SingleTickerProviderStateMixin {
   late AnimationController _controller;
-  late Animation<double> _positionAnimation;
-  late Animation<double> _bounceAnimation;
-  double screenHeight = 0.0;
+  late Animation<double> _fadeAnimation;
 
   @override
   void initState() {
@@ -22,31 +20,21 @@ class ImageRiseAnimationState extends State<ImageRiseAnimation>
 
     _controller = AnimationController(
       vsync: this,
-      duration: Duration(seconds: 2),
+      duration: Duration(seconds: 2), // 2초 동안 페이드 인
+    );
+
+    _fadeAnimation = Tween<double>(begin: 0.0, end: 1.0).animate(
+      CurvedAnimation(parent: _controller, curve: Curves.easeIn),
     );
 
     Future.delayed(Duration(milliseconds: 500), () {
       if (mounted) {
-        setState(() {
-          screenHeight = MediaQuery.of(context).size.height;
-          double startPosition = screenHeight + 80;
-          double endPosition = (screenHeight / 2) - 50;
-
-          _positionAnimation = Tween<double>(begin: startPosition, end: endPosition).animate(
-            CurvedAnimation(parent: _controller, curve: Curves.easeOut),
+        _controller.forward().then((_) {
+          // 애니메이션이 끝나면 홈 화면으로 전환
+          Navigator.pushReplacement(
+            context,
+            MaterialPageRoute(builder: (context) => MyHomePage()), // main.dart의 홈 화면
           );
-
-          _bounceAnimation = Tween<double>(begin: 0, end: -10).animate(
-            CurvedAnimation(parent: _controller, curve: Curves.elasticOut),
-          );
-
-          _controller.forward().then((_) {
-            // 애니메이션이 끝난 후 홈 화면으로 전환
-            Navigator.pushReplacement(
-              context,
-              MaterialPageRoute(builder: (context) => MyHomePage()), // main.dart의 홈 화면
-            );
-          });
         });
       }
     });
@@ -62,43 +50,19 @@ class ImageRiseAnimationState extends State<ImageRiseAnimation>
   Widget build(BuildContext context) {
     final themeProvider = Provider.of<ThemeProvider>(context);
 
-    if (screenHeight == 0.0) {
-      return Scaffold(
-        backgroundColor: ColorPalette.palette[themeProvider.selectedThemeIndex][0],
-        body: Center(child: CircularProgressIndicator()),
-      );
-    }
-
     return Scaffold(
       backgroundColor: ColorPalette.palette[themeProvider.selectedThemeIndex][0],
-      body: Stack(
-        alignment: Alignment.center,
-        children: [
-          // 터치 방지용 오버레이
-          Positioned.fill(
-            child: IgnorePointer(
-              ignoring: _controller.isAnimating, // 애니메이션 중에는 터치 방지
-              child: Container(color: Colors.transparent),
-            ),
+      body: Center(
+        child: FadeTransition(
+          opacity: _fadeAnimation, // 서서히 나타나는 효과
+          child: Image.asset(
+            'assets/title/logo.png',
+            width: 150,
+            height: 150,
           ),
-          AnimatedBuilder(
-            animation: _controller,
-            builder: (context, child) {
-              return Positioned(
-                top: _positionAnimation.value + _bounceAnimation.value,
-                child: Opacity(
-                  opacity: _controller.value,
-                  child: Image.asset(
-                    'assets/title/logo.png',
-                    width: 150,
-                    height: 150,
-                  ),
-                ),
-              );
-            },
-          ),
-        ],
+        ),
       ),
     );
   }
 }
+
