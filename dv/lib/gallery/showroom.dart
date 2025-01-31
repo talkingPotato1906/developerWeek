85a153efcc9dc290f31b52e0277fd9d622ff127d
Eunjin <eunjinlee0128@gmail.com>
2025-02-01 04:05:19 +0900

    ✨ 로그아웃 상태에서 갤러리에 로그인 필요 문구 삽입
    로그아웃 상태에서 갤러리를 볼 경우 로그인 필요 문구 뜨도록 추가

--- dv/lib/gallery/showroom.dart
+++ dv/lib/gallery/showroom.dart
@@ -1,3 +1,4 @@
+import 'package:dv/login/login_provider.dart';
 import 'package:dv/settings/language/language_provider.dart';
 import 'package:dv/settings/theme/color_palette.dart';
 import 'package:dv/settings/theme/theme_provider.dart';
@@ -11,11 +12,13 @@ class ShowroomPage extends StatelessWidget {
 
   void _showImageContent(BuildContext context, Map<String, dynamic> imageData) {
     final themeProvider = Provider.of<ThemeProvider>(context, listen: false);
+
     showDialog(
       context: context,
       builder: (context) {
         return Dialog(
-          backgroundColor: ColorPalette.palette[themeProvider.selectedThemeIndex][0],
+          backgroundColor:
+              ColorPalette.palette[themeProvider.selectedThemeIndex][0],
           shape: RoundedRectangleBorder(
             borderRadius: BorderRadius.circular(16.0),
           ),
@@ -70,43 +73,53 @@ class ShowroomPage extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-
     final languageProvider = Provider.of<LanguageProvider>(context);
+    final loginProvider = Provider.of<LogInProvider>(context, listen: false);
 
     return Consumer<ImageProviderClass>(
       builder: (context, provider, child) {
         final selectedImages = provider.selectedImages;
 
         return Scaffold(
-          appBar: AppBar(title: Text(languageProvider.getLanguage(message: "갤러리")),
-          automaticallyImplyLeading: false,),
-          body: selectedImages.isEmpty
-              ? Center(child: Text(languageProvider.getLanguage(message: "갤러리가 비어있습니다")))
-              : Padding(
-                  padding: const EdgeInsets.all(8.0),
-                  child: GridView.builder(
-                    gridDelegate: SliverGridDelegateWithFixedCrossAxisCount(
-                      crossAxisCount: 3,
-                      crossAxisSpacing: 8.0,
-                      mainAxisSpacing: 8.0,
-                    ),
-                    itemCount: selectedImages.length,
-                    itemBuilder: (context, index) {
-                      final imageData = selectedImages[index];
-                      return GestureDetector(
-                        onTap: () => _showImageContent(context, imageData),
-                        child: ClipRRect(
-                          borderRadius: BorderRadius.circular(8.0),
-                          child: Image.memory(
-                            imageData["image"],
-                            fit: BoxFit.cover,
+            appBar: AppBar(
+              title: Text(languageProvider.getLanguage(message: "갤러리")),
+              automaticallyImplyLeading: false,
+            ),
+            body: loginProvider.isLoggedIn
+                ? selectedImages.isEmpty
+                    ? Center(
+                        child: Text(languageProvider.getLanguage(
+                            message: "갤러리가 비어있습니다")))
+                    : Padding(
+                        padding: const EdgeInsets.all(8.0),
+                        child: GridView.builder(
+                          gridDelegate:
+                              SliverGridDelegateWithFixedCrossAxisCount(
+                            crossAxisCount: 3,
+                            crossAxisSpacing: 8.0,
+                            mainAxisSpacing: 8.0,
                           ),
+                          itemCount: selectedImages.length,
+                          itemBuilder: (context, index) {
+                            final imageData = selectedImages[index];
+                            return GestureDetector(
+                              onTap: () =>
+                                  _showImageContent(context, imageData),
+                              child: ClipRRect(
+                                borderRadius: BorderRadius.circular(8.0),
+                                child: Image.memory(
+                                  imageData["image"],
+                                  fit: BoxFit.cover,
+                                ),
+                              ),
+                            );
+                          },
                         ),
-                      );
-                    },
-                  ),
-                ),
-        );
+                      )
+                : Center(
+                    child: Text(languageProvider.getLanguage(
+                        message: "로그인이 필요한 서비스입니다.")),
+                  ));
       },
     );
   }
