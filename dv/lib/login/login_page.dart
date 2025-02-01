9523f9888e65004d1581310ec21f85729c8f66f9
Eunjin <eunjinlee0128@gmail.com>
2025-02-01 20:29:52 +0900

    ✨ 로그인 창에 메뉴 버튼 추가
    로그인 창에 메뉴 버튼 추가

--- dv/lib/login/login_page.dart
+++ dv/lib/login/login_page.dart
@@ -1,5 +1,6 @@
 import 'package:dv/login/login_provider.dart';
 import 'package:dv/login/login_success_page.dart';
+import 'package:dv/menu/menu.dart';
 import 'package:dv/settings/language/language_provider.dart';
 import 'package:dv/settings/theme/color_palette.dart';
 import 'package:dv/settings/theme/theme_provider.dart';
@@ -62,97 +63,102 @@ class _LoginPageState extends State<LoginPage> {
 
     return Scaffold(
       appBar: AppBar(title: Text(languageProvider.getLanguage(message: "로그인"))),
-      body: Padding(
-        padding: EdgeInsets.all(16.0),
-        child: Form(
-          key: _formKey,
-          child: Padding(
-            padding: const EdgeInsets.only(left: 30, right: 30),
-            child: Column(
-              mainAxisAlignment: MainAxisAlignment.center,
-              children: [
-                TextFormField(
-                  controller: emailController,
-                  decoration: InputDecoration(
-                    labelText: languageProvider.getLanguage(message: "이메일"),
-                    labelStyle: TextStyle(
-                        color: themeProvider
-                            .getTheme()
-                            .textTheme
-                            .bodyMedium
-                            ?.color),
-                    enabledBorder: UnderlineInputBorder(
-                      borderSide: BorderSide(
-                          color: ColorPalette
-                              .palette[themeProvider.selectedThemeIndex][3]),
+      body: Stack(
+        children: [
+          FloatingMenuButton(), //  메뉴 버튼 삽입
+          Padding(
+          padding: EdgeInsets.all(16.0),
+          child: Form(
+            key: _formKey,
+            child: Padding(
+              padding: const EdgeInsets.only(left: 30, right: 30),
+              child: Column(
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: [
+                  TextFormField(
+                    controller: emailController,
+                    decoration: InputDecoration(
+                      labelText: languageProvider.getLanguage(message: "이메일"),
+                      labelStyle: TextStyle(
+                          color: themeProvider
+                              .getTheme()
+                              .textTheme
+                              .bodyMedium
+                              ?.color),
+                      enabledBorder: UnderlineInputBorder(
+                        borderSide: BorderSide(
+                            color: ColorPalette
+                                .palette[themeProvider.selectedThemeIndex][3]),
+                      ),
+                      focusedBorder: UnderlineInputBorder(
+                        borderSide: BorderSide(
+                            color: ColorPalette
+                                .palette[themeProvider.selectedThemeIndex][3]),
+                      ),
                     ),
-                    focusedBorder: UnderlineInputBorder(
-                      borderSide: BorderSide(
-                          color: ColorPalette
-                              .palette[themeProvider.selectedThemeIndex][3]),
+                    keyboardType: TextInputType.emailAddress,
+                    validator: (value) {
+                      if (value == null || value.isEmpty) {
+                        return languageProvider.getLanguage(
+                            message: "이메일을 입력하세요");
+                      }
+                      return null;
+                    },
+                  ),
+                  SizedBox(height: 16),
+                  TextFormField(
+                    controller: passwordController,
+                    decoration: InputDecoration(
+                      labelText: languageProvider.getLanguage(message: "비밀번호"),
+                      labelStyle: TextStyle(
+                          color: themeProvider
+                              .getTheme()
+                              .textTheme
+                              .bodyMedium
+                              ?.color),
+                      enabledBorder: UnderlineInputBorder(
+                        borderSide: BorderSide(
+                            color: ColorPalette
+                                .palette[themeProvider.selectedThemeIndex][3]),
+                      ),
+                      focusedBorder: UnderlineInputBorder(
+                        borderSide: BorderSide(
+                            color: ColorPalette
+                                .palette[themeProvider.selectedThemeIndex][3]),
+                      ),
+                    ),
+                    obscureText: true,
+                    validator: (value) {
+                      if (value == null || value.isEmpty) {
+                        return languageProvider.getLanguage(
+                            message: "비밀번호를 입력하세요");
+                      }
+                      return null;
+                    },
+                  ),
+                  SizedBox(height: 28),
+                  ElevatedButton(
+                    onPressed: _login,
+                    style: ElevatedButton.styleFrom(
+                        backgroundColor: ColorPalette
+                            .palette[themeProvider.selectedThemeIndex][3],
+                        padding:
+                            EdgeInsets.symmetric(horizontal: 30, vertical: 15)),
+                    child: Text(
+                      languageProvider.getLanguage(message: "로그인"),
+                      style: TextStyle(
+                        color: ColorPalette
+                            .palette[themeProvider.selectedThemeIndex][0],
+                        fontSize: 18,
+                      ),
                     ),
                   ),
-                  keyboardType: TextInputType.emailAddress,
-                  validator: (value) {
-                    if (value == null || value.isEmpty) {
-                      return languageProvider.getLanguage(
-                          message: "이메일을 입력하세요");
-                    }
-                    return null;
-                  },
-                ),
-                SizedBox(height: 16),
-                TextFormField(
-                  controller: passwordController,
-                  decoration: InputDecoration(
-                    labelText: languageProvider.getLanguage(message: "비밀번호"),
-                    labelStyle: TextStyle(
-                        color: themeProvider
-                            .getTheme()
-                            .textTheme
-                            .bodyMedium
-                            ?.color),
-                    enabledBorder: UnderlineInputBorder(
-                      borderSide: BorderSide(
-                          color: ColorPalette
-                              .palette[themeProvider.selectedThemeIndex][3]),
-                    ),
-                    focusedBorder: UnderlineInputBorder(
-                      borderSide: BorderSide(
-                          color: ColorPalette
-                              .palette[themeProvider.selectedThemeIndex][3]),
-                    ),
-                  ),
-                  obscureText: true,
-                  validator: (value) {
-                    if (value == null || value.isEmpty) {
-                      return languageProvider.getLanguage(
-                          message: "비밀번호를 입력하세요");
-                    }
-                    return null;
-                  },
-                ),
-                SizedBox(height: 28),
-                ElevatedButton(
-                  onPressed: _login,
-                  style: ElevatedButton.styleFrom(
-                      backgroundColor: ColorPalette
-                          .palette[themeProvider.selectedThemeIndex][3],
-                      padding:
-                          EdgeInsets.symmetric(horizontal: 30, vertical: 15)),
-                  child: Text(
-                    languageProvider.getLanguage(message: "로그인"),
-                    style: TextStyle(
-                      color: ColorPalette
-                          .palette[themeProvider.selectedThemeIndex][0],
-                      fontSize: 18,
-                    ),
-                  ),
-                ),
-              ],
+                ],
+              ),
             ),
           ),
         ),
+        ]
       ),
     );
   }
