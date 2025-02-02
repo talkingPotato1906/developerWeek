d6fc552cf3a2f4a6d9d5f55a60f4dca174bdd0ae
suminJeong <jsm24@sookmyung.ac.kr>
2025-02-03 01:39:25 +0900

    ✨ 사용자 계정 정보

---
+++ dv/lib/sign_up/account_list.dart
@@ -0,0 +1,8 @@
+//기본 유저 정보
+//임시정보
+class AccountList {
+  static Map<String, String> userInformation = {
+    "test@example.com": "123456", //순서대로 이메일, 비번
+    "test1@example.com": "123456"
+  };
+}
