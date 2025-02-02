0addcc439bdabcc1a1f1b574a0878df14ce59e9a
Eunjin <eunjinlee0128@gmail.com>
2025-02-02 21:48:50 +0900

    ✨ 마이 페이지 안으로 설정 버튼 이동
    마이 페이지 안으로 설정 버튼 이동. 설정 창 내에서 변경된 사항 마이 페이지에서 반영 뒤 페이지 빌드.

--- dv/lib/mypage/my_page_screen.dart
+++ dv/lib/mypage/my_page_screen.dart
@@ -1,5 +1,6 @@
 import 'package:dv/menu/menu.dart';
 import 'package:dv/settings/language/language_provider.dart';
+import 'package:dv/settings/setting_screen.dart';
 import 'package:dv/settings/theme/color_palette.dart';
 import 'package:dv/settings/theme/theme_provider.dart';
 import 'package:flutter/material.dart';
@@ -13,14 +14,20 @@ class MyPageScreen extends StatefulWidget {
 }
 
 class _MyPageScreenState extends State<MyPageScreen> {
-  String nickname = "nickname"; //유저 닉네임(입력받으면 바꿔야 함)
-  int point = 0; //포인트 초기값 설정
+  String nickname = "nickname"; // 유저 닉네임 (입력받으면 변경)
+  int point = 0; // 포인트 초기값
+  late LanguageProvider languageProvider;
+
+  @override
+  void didChangeDependencies() {
+    super.didChangeDependencies();
+    languageProvider = Provider.of<LanguageProvider>(context, listen: false);
+    setState(() {});
+  }
 
   @override
   Widget build(BuildContext context) {
     final imageSize = MediaQuery.of(context).size.width / 8;
-    final languageProvider =
-        Provider.of<LanguageProvider>(context, listen: false);
     final themeProvider = Provider.of<ThemeProvider>(context, listen: false);
 
     return Scaffold(
@@ -30,99 +37,107 @@ class _MyPageScreenState extends State<MyPageScreen> {
       floatingActionButton: FloatingMenuButton(),
       body: SingleChildScrollView(
         child: Padding(
-          padding: const EdgeInsets.all(16.0),
-          child: Column(
-            children: [
-              Container(
-                constraints: BoxConstraints(
-                  //위젯 크기 제약 조건 설정
-                  minHeight: MediaQuery.of(context).size.width,
-                  minWidth: MediaQuery.of(context).size.width,
-                ),
-                child: Align(
-                  alignment: Alignment.topLeft,
-                  child: Padding(
-                    padding: const EdgeInsets.only(left: 100, top: 70),
-                    child: Column(
-                      crossAxisAlignment: CrossAxisAlignment.start, //왼쪽 정렬
+          padding: const EdgeInsets.all(25.0),
+          child: Container(
+            width: double.infinity, // 최대한 화면 크기에 맞게 설정
+            padding: const EdgeInsets.symmetric(horizontal: 16),
+            child: Column(
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: [
+                Row(
+                  crossAxisAlignment: CrossAxisAlignment.center,
+                  children: [
+                    // 프로필 아이콘 및 버튼
+                    Column(
                       children: [
-                        Row(
-                          children: [
-                            Column(
-                              children: [
-                                Icon(
-                                  Icons.account_circle,
-                                  size: imageSize,
-                                  color: ColorPalette
-                                      .palette[themeProvider.selectedThemeIndex]
-                                          [3]
-                                      .withAlpha(128),
-                                ),
-                                SizedBox(
-                                  height: 10,
-                                ),
-                                ElevatedButton(
-                                  onPressed: () {
-                                    
-                                  },
-                                  style: ElevatedButton.styleFrom(
-                                      backgroundColor: ColorPalette.palette[
-                                          themeProvider.selectedThemeIndex][3]),
-                                  child: Text(
-                                    languageProvider.getLanguage(message: "프로필 편집"),
-                                    style: TextStyle(
-                                        color: ColorPalette.palette[
-                                            themeProvider
-                                                .selectedThemeIndex][0]),
-                                  ),
-                                ),
-                              ],
+                        Icon(
+                          Icons.account_circle,
+                          size: imageSize,
+                          color: ColorPalette
+                              .palette[themeProvider.selectedThemeIndex][3]
+                              .withAlpha(128),
+                        ),
+                        const SizedBox(height: 10),
+                        ElevatedButton(
+                          onPressed: () {},
+                          style: ElevatedButton.styleFrom(
+                            backgroundColor: ColorPalette
+                                .palette[themeProvider.selectedThemeIndex][3],
+                          ),
+                          child: Text(
+                            languageProvider.getLanguage(message: "프로필 편집"),
+                            style: TextStyle(
+                              color: ColorPalette
+                                  .palette[themeProvider.selectedThemeIndex][0],
                             ),
-                            SizedBox(
-                              width: 30,
-                            ),
-                            Column(
-                              crossAxisAlignment: CrossAxisAlignment.start,
-                              children: [
-                                Text(
-                                  nickname,
-                                  style: TextStyle(
-                                    fontSize: 25,
-                                  ),
-                                ),
-                                SizedBox(
-                                  height: 7,
-                                ),
-                                Row(
-                                  children: [
-                                    Text(
-                                      languageProvider.getLanguage(message: "보유 포인트"),
-                                      style: TextStyle(
-                                        fontSize: 18,
-                                        color: ColorPalette.palette[
-                                            themeProvider.selectedThemeIndex][2],
-                                      ),
-                                    ),
-                                    Text(
-                                      " : $point pt",
-                                      style: TextStyle(
-                                        fontSize: 18,
-                                        color: ColorPalette.palette[
-                                            themeProvider.selectedThemeIndex][2],
-                                      ),
-                                    ),
-                                  ],
-                                ),
-                              ],
-                            ),
-                          ],
+                          ),
                         ),
                       ],
                     ),
-                  ), //왼쪽 상단 정렬
+                    const SizedBox(width: 16),
+
+                    // 닉네임 및 포인트 정보
+                    Expanded(
+                      child: Column(
+                        crossAxisAlignment: CrossAxisAlignment.start,
+                        children: [
+                          Text(
+                            nickname,
+                            style: const TextStyle(
+                              fontSize: 25,
+                              fontWeight: FontWeight.bold,
+                            ),
+                          ),
+                          const SizedBox(height: 7),
+                          Row(
+                            children: [
+                              Text(
+                                languageProvider.getLanguage(message: "보유 포인트"),
+                                style: TextStyle(
+                                  fontSize: 18,
+                                  color: ColorPalette.palette[
+                                      themeProvider.selectedThemeIndex][2],
+                                ),
+                              ),
+                              Text(
+                                " : $point pt",
+                                style: TextStyle(
+                                  fontSize: 18,
+                                  color: ColorPalette.palette[
+                                      themeProvider.selectedThemeIndex][2],
+                                ),
+                              ),
+                            ],
+                          ),
+                        ],
+                      ),
+                    ),
+
+                    // 설정 아이콘 버튼
+                    IconButton(
+                      //  settings/setting_screen.dart에서 뒤로가기 누르면 감지하고 변경 내용 반영
+                      onPressed: () async {
+                        final result = await Navigator.push(
+                            context,
+                            MaterialPageRoute(
+                              builder: (context) => SettingScreen(),
+                            ));
+
+                        if (result == true) {
+                          setState(() {});
+                        }
+                      },
+                      icon: Icon(
+                        Icons.settings,
+                        color: ColorPalette
+                            .palette[themeProvider.selectedThemeIndex][2],
+                        size: 36,
+                      ),
+                    ),
+                  ],
                 ),
-              ),
-            ],
+              ],
+            ),
           ),
         ),
       ),
--- dv/lib/settings/language/language_provider.dart
+++ dv/lib/settings/language/language_provider.dart
@@ -2,16 +2,16 @@ import 'package:dv/settings/language/language_list.dart';
 import 'package:flutter/material.dart';
 import 'package:shared_preferences/shared_preferences.dart';
 
-class LanguageProvider extends ChangeNotifier{
+class LanguageProvider extends ChangeNotifier {
   int _selectedLanguageIndex = 0;
 
   int get selectedLanguageIndex => _selectedLanguageIndex;
 
-  LanguageProvider(){
+  LanguageProvider() {
     loadLanguage();
   }
 
-  Future<void> changeLanguage(int index) async{
+  Future<void> changeLanguage(int index) async {
     _selectedLanguageIndex = index;
     notifyListeners();
 
@@ -19,20 +19,24 @@ class LanguageProvider extends ChangeNotifier{
     await prefs.setInt("selectedLanguageIndex", index);
   }
 
-  Future<void> loadLanguage() async{
+  Future<void> loadLanguage() async {
     SharedPreferences prefs = await SharedPreferences.getInstance();
     _selectedLanguageIndex = prefs.getInt("selectedLanguageIndex") ?? 0;
     notifyListeners();
   }
 
-  String getLanguage({required String? message}){
-    if (message == null || !LanguageList.languageMessages.containsKey(message)) {
-      return message ?? "Unknown"; // Fallback value if message is null or not found
+  String getLanguage({required String? message}) {
+    if (message == null ||
+        !LanguageList.languageMessages.containsKey(message)) {
+      return message ??
+          "Unknown"; // Fallback value if message is null or not found
     }
 
     List<String> translations = LanguageList.languageMessages[message] ?? [];
     if (_selectedLanguageIndex >= translations.length) {
-      return translations.isNotEmpty ? translations[0] : "Unknown"; // Fallback to default language
+      return translations.isNotEmpty
+          ? translations[0]
+          : "Unknown"; // Fallback to default language
     }
 
     return translations[_selectedLanguageIndex];
